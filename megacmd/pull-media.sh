05e9302e32be1dd52a450902c511cf39826c7699
Matt Lewis <matt@m-lewis.co.uk>
2026-07-06 00:01:40 +0100

    pull-media.sh: fix variable check. fix local file movement.

--- megacmd/pull-media.sh
+++ megacmd/pull-media.sh
@@ -1,11 +1,17 @@
 # check required variables have been set
-if ! [ -v MEGA_IN_FOLDER ]; then echo "Error: MEGA_IN_FOLDER is not set. Exiting."; exit 1; fi 
-if ! [ -v MEGA_OUT_FOLDER ]; then echo "Error: MEGA_OUT_FOLDER is not set. Exiting."; exit 1; fi
+if [ -z ${MEGA_IN_FOLDER+x} ]; then
+  echo "Error: MEGA_IN_FOLDER is not set. Exiting."
+  exit 1
+fi
+if [ -z ${MEGA_OUT_FOLDER+x} ]; then
+  echo "Error: MEGA_OUT_FOLDER is not set. Exiting."
+  exit 1
+fi
 
 # check if any files have been added
 mega-ls "$MEGA_IN_FOLDER" | while read -r FILE; do
-        echo "Processing: ${MEGA_IN_FOLDER}/${FILE}" >> /var/log/cron.log
-        mega-get "${MEGA_IN_FOLDER}/${FILE}" /temp-data
-        mv /temp-data/${FILE} /mega-out/${FILE}
-        mega-mv "${MEGA_IN_FOLDER}/${FILE}" "${MEGA_OUT_FOLDER}"
+  echo "Processing: ${MEGA_IN_FOLDER}/${FILE}" >>/var/log/cron.log
+  mega-get "${MEGA_IN_FOLDER}/${FILE}" /temp-data
+  mv /temp-data/${FILE} /mega-out
+  mega-mv "${MEGA_IN_FOLDER}/${FILE}" "${MEGA_OUT_FOLDER}"
 done
