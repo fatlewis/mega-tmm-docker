f802de75c88db7820cdcd10f3228b362ddeed561
Matthew Lewis <matt@m-lewis.co.uk>
2026-09-12 21:18:15 +0100

    feat: Call mega-mkdir and run TMM commands only when necessary

--- megacmd/pull-media.sh
+++ megacmd/pull-media.sh
@@ -7,18 +7,25 @@ fi
 
 for SUBFOLDER in "movies" "tv_shows" "videos"; do
   # ensure source and destination folders have the necessary subdirectories
-  mega-mkdir "${SRC_FOLDER}/${SUBFOLDER}"
+  if ! mega-ls "${SRC_FOLDER}" | grep -q ^${SUBFOLDER}$; then
+    mega-mkdir "${SRC_FOLDER}/${SUBFOLDER}"
+  fi
   mkdir /mega-out/${SUBFOLDER}
 
   # fetch any new files. grep -v : strips the root directory out of the file list.
-  mega-ls "$SRC_FOLDER/${SUBFOLDER}" | grep -v : | while read -r FILE; do
+  NUM_FILES=$(mega-ls "${SRC_FOLDER}/${SUBFOLDER}" | grep -v : | wc -l)
+  mega-ls "${SRC_FOLDER}/${SUBFOLDER}" | grep -v : | while read -r FILE; do
     echo "Processing: ${SRC_FOLDER}/${SUBFOLDER}/${FILE}" >>/var/log/cron.log
     mega-get "${SRC_FOLDER}/${SUBFOLDER}/${FILE}" /temp-data
     mv "/temp-data/${FILE}" /mega-out/${SUBFOLDER}
     mega-rm "${SRC_FOLDER}/${SUBFOLDER}/${FILE}"
   done
-done
 
-# run tinyMediaManger to process new files
-/root/tinyMediaManager/tinyMediaManager movie -u -n -r
-/root/tinyMediaManager/tinyMediaManager tvshow -u -n -r
+  # Run tmm if movies or tv_shows and there were new files
+  if test $NUM_FILES != "0" && test $SUBFOLDER = "movies"; then
+    /root/tinyMediaManager/tinyMediaManager movie -u -n -r
+  fi
+  if test $NUM_FILES != "0" && test $SUBFOLDER = "tv_shows"; then
+    /root/tinyMediaManager/tinyMediaManager tvshow -u -n -r
+  fi
+done
